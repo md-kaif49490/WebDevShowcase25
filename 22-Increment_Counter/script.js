77488eb6dcdcb9cbb1938b49b3134c605da697a2
Md kaiful Haque <kaif49490@gmail.com>
2024-06-08 13:56:26 +0530

    Increment Counter

---
+++ 22-Increment_Counter/script.js
@@ -0,0 +1,22 @@
+const counters = document.querySelectorAll('.counter');
+ 
+counters.forEach((counter) => {
+
+  counter.innerText = '0'
+
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const c = +counter.innerText
+
+    const increment = target / 200
+
+    if (c < target) {
+      counter.innerText =  `${Math.ceil(c + increment)}`
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  }
+
+  updateCount()
+});
